15801adb60eb2af1784b063ecf9dcba71f213365
shree1113 <148195257+shree1113@users.noreply.github.com>
2025-10-14 15:00:18 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -17,9 +17,6 @@
 """
 Test Factory to make fake objects for testing
 """
-import factory
-from factory.fuzzy import FuzzyChoice, FuzzyDecimal
-from service.models import Product, Category
 
 
 class ProductFactory(factory.Factory):
